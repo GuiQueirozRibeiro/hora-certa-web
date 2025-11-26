f139b7c61b2ab6eead4ee65ed8420a1fa900c0bb
Guilherme Rodrigues <116519824+gui-ccr@users.noreply.github.com>
2025-11-26 16:53:14 -0300

    removendo site da empresa puro em react

--- agendai-empresa/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import DashboardPage from './pages/DashboardPage';
-// import LoginPage from './pages/LoginPage';
-
-export default function App() {
-  return <DashboardPage />;
-  // return <LoginPage />;
-}
