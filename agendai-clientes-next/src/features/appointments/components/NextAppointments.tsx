7974c6da262568a6db350fc5d3f2b11842a56958
Guilherme Rodrigues <116519824+gui-ccr@users.noreply.github.com>
2026-01-05 13:53:19 -0300

    os cards de nextApoointment agora ficam em linha

--- agendai-clientes-next/src/features/appointments/components/NextAppointments.tsx
+++ agendai-clientes-next/src/features/appointments/components/NextAppointments.tsx
@@ -49,7 +49,7 @@ export default function NextAppointments() {
       </div>
 
       {/* Lista de Agendamentos */}
-      <div className='pr-220'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
         {upcomingAppointments.map((appointment) => (
           <AppointmentCard
             key={appointment.id}
